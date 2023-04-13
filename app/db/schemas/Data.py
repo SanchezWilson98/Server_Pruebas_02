270fb58bc7b18ce947c7a52ff7dcc651c8d66ff6
SanchezWilson98 <49734067+SanchezWilson98@users.noreply.github.com>
2023-04-13 18:20:36 -0500

    Update Data.py

--- app/db/schemas/Data.py
+++ app/db/schemas/Data.py
@@ -1,6 +1,8 @@
 def data_schemas(data) -> dict:
     return {"id": str(data["_id"]),
             "name": data["name"],
-            "node": data["node"],
-            "data": data["data"],
+            "data0": data["data0"],
+            "data1": data["data1"],
+            "data2": data["data2"],
+            "data3": data["data3"],
             "time": data["time"]}
