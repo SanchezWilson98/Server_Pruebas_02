654def99dc9c46ff39c7bf20c5a566dffa7e8d62
SanchezWilson98 <49734067+SanchezWilson98@users.noreply.github.com>
2023-04-13 18:19:19 -0500

    Update data.py

--- app/db/models/data.py
+++ app/db/models/data.py
@@ -3,9 +3,11 @@ from typing import Union
 
 class Data(BaseModel):
     name : str
-    node : str
-    data: str
-
+    data0: str
+    data1: str
+    data2: str
+    data3: str
+        
 class Name(BaseModel):
     DATA: (Union[Data, None]) = None
 
